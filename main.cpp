42b1e3521d595b3bdd26f2bb5ce13e0b6f9ea81f
Nutifafa Afi Attor <attorfafa@gmail.com>
2021-08-09 15:32:09 +0000

    I just added a function to add two numbers

--- main.cpp
+++ main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+int addition(int a, int b)
+{
+ return (a+b);
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+     int a, b, answer;
+     cout<<"Enter first number: ";
+     cin>> a;
+     cout<<"Enter second number: ";
+     cin>> b;
+
+     answer = addition(a,b);
+     cout << answer;
     return 0;
 }
+
